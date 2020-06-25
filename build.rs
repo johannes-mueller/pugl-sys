fae7d78439db420fd03e9d214bcffdf5b377c592
Johannes Mueller <github@johannes-mueller.org>
2020-06-25 13:16:15 +0200

    Compile pugl into OUT_DIR and clean waflib dir after building

--- build.rs
+++ build.rs
@@ -1,29 +1,31 @@
 extern crate bindgen;
 
 use std::process::Command;
-
+use std::fs;
 use std::env;
 use std::path::PathBuf;
 
 fn main() {
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
 
     Command::new("python")
-	.current_dir("pugl")
-	.env("CFLAGS", "-fPIC")
-	.arg("waf")
+        .current_dir("pugl")
+        .env("CFLAGS", "-fPIC")
+        .arg("waf")
         .arg("configure")
-	.status()
+        .arg(format!("--out={}", out_path.to_str().unwrap()))
+        .status()
         .expect("waf configure failed");
 
     Command::new("python")
-	.current_dir("pugl")
-	.env("CFLAGS", "-fPIC")
-	.arg("waf")
+        .current_dir("pugl")
+        .env("CFLAGS", "-fPIC")
+        .arg("waf")
         .arg("build")
-	.status()
+        .status()
         .expect("waf build failed");
 
-    println!("cargo:rustc-link-search=pugl/build");
+    println!("cargo:rustc-link-search={}", out_path.to_str().unwrap());
     println!("cargo:rustc-link-lib=static=pugl_x11-0");
     println!("cargo:rustc-link-lib=static=pugl_x11_cairo-0");
     println!("cargo:rustc-flags=-l cairo -l GLU -l GL -lX11 -lXext -lXrandr -lXcursor");
@@ -34,14 +36,18 @@ fn main() {
     let bindings = bindgen::Builder::default()
         .header("pugl/pugl/pugl.h")
         .header("pugl/pugl/pugl_cairo.h")
-	.clang_arg("-Ipugl")
-	.parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .clang_arg("-Ipugl")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .generate()
         .expect("Unable to generate bindings");
 
-    // Write the bindings to the $OUT_DIR/bindings.rs file.
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
+
+
+    let _ = fs::remove_dir_all("pugl/waflib/__pycache__");
+    let _ = fs::remove_dir_all("pugl/waflib/Tools/__pycache__");
+    let _ = fs::remove_dir_all("pugl/waflib/extras/__pycache__");
+    let _ = fs::remove_file("pugl/.lock-waf_linux_build");
 }
